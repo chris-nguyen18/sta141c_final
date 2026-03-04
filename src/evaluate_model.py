5c11435f2c755608ce20fa98a6a1772112087469
chris-nguyen18 <chris.nguyensj04@gmail.com>
2026-03-04 14:24:31 -0800

    model training and partial evaluation

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -1,13 +1,61 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import joblib
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+from sklearn.metrics import (
+   accuracy_score, precision_score, 
+   recall_score, f1_score, roc_auc_score, 
+   confusion_matrix, ConfusionMatrixDisplay
+   )
 
-def evaluate_model(model, X_test, y_test):
+def evaluate_model(model, X_test, y_test, output_name):
+   #model = joblib.load(model_path)
+
    y_pred = model.predict(X_test)
+   y_proba = model.predict_proba(X_test)[:, 1]
 
    metrics = {
-      "accuracy": accuracy_score(y_test, y_pred),
-      "precision": precision_score(y_test, y_pred),
-      "recall": recall_score(y_test, y_pred),
-      "f1": f1_score(y_test, y_pred),
+      "Accuracy": accuracy_score(y_test, y_pred),
+      "Precision": precision_score(y_test, y_pred),
+      "Recall": recall_score(y_test, y_pred),
+      "F1": f1_score(y_test, y_pred),
+      "ROC-AUC": roc_auc_score(y_test, y_proba)
    }
 
+   
+   cm = confusion_matrix(y_test, y_pred)
+   disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+   disp.plot()
+   plt.title(f"Confusion Matrix: {output_name}")
+   plt.savefig(f"../reports/{output_name}_cm.png")
+   plt.close()
+
    return metrics
+
+if __name__ == "__main__":
+   # Load test data
+   X_test = np.load("../data/processed/X_test.npy")
+   y_test = np.load("../data/processed/y_test.npy")
+
+   # Load trained model
+   models = {
+      "logistic_raw": "../models/logistic_raw.joblib",
+      "logistic_pca": "../models/logistic_pca.joblib",
+      "random_forest": "../models/random_forest.joblib"
+   } 
+
+   
+   for name, path in models.items():
+      print(f"\nEvaluating {name}...")
+      model = joblib.load(path)
+
+      if hasattr(model, "predict"):
+         metrics = evaluate_model(model, X_test, y_test, name)
+
+         for k, v in metrics.items():
+            print(f"{k}: {v}")
+      else:
+        # This will tell you exactly why it's skipping
+        print(f"SKIPPED: {name} is actually a {type(model).__name__}")
+ 
--- src/train_model.py
+++ src/train_model.py
@@ -1,56 +1,46 @@
-from feature_engineering import (
-   load_processed_data,
-   scale_features,
-   apply_pca
-)
-
+import os
+import joblib
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-import joblib
-import os
 
-def train_logistic(X_train, y_train):
-   model = LogisticRegression(max_iter=1000)
-   model.fit(X_train, y_train)
-   return model
-
-def train_random_forest(X_train, y_train):
-   model = RandomForestClassifier(
-      n_estimators=100,
-      random_state=67,
-      n_jobs=-1
-   )
-   model.fit(X_train, y_train)
-   return model
+from feature_engineering import load_processed_data
 
 def save_model(model, filename):
    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
    model_dir = os.path.join(base_dir, "models")
-
    os.makedirs(model_dir, exist_ok=True)
    joblib.dump(model, os.path.join(model_dir, filename))
 
 if __name__ == "__main__":
-   # Load data and scale
    X_train, X_test, y_train, y_test = load_processed_data()
-   X_train_scaled, X_test_scaled, scaler = scale_features(X_train, X_test)
 
-   save_model(scaler, "scaler.joblib")
+   # Logistic Regression Only
+   pipe_logistic_raw = Pipeline([
+      ('scaler', StandardScaler()),
+      ('classifier', LogisticRegression(max_iter=1000, class_weight="balanced"))
+   ])
+   pipe_logistic_raw.fit(X_train, y_train)
+   save_model(pipe_logistic_raw, "logistic_raw.joblib")
 
-   # Train logistic (no PCA)
-   log_model_raw = train_logistic(X_train_scaled, y_train)
-   save_model(scaler, "scaler.joblib")
-   
-   # Apply PCA
-   X_train_pca, X_test_pca, pca = apply_pca(X_train_scaled, X_test_scaled)
-   save_model(pca, "pca.joblib")
-   
-   # Train logistic (PCA)
-   log_model_pca = train_logistic(X_train_pca, y_train)
-   save_model(log_model_pca, "logistic_pca.joblib")
+   # Logistic Regression w/ PCA
+   # Scaler + PCA + Model
+   pipe_logistic_pca = Pipeline([
+      ('scaler', StandardScaler()),
+      ('pca', PCA(n_components=15)),
+      ('classifier', LogisticRegression(max_iter=1000, class_weight="balanced"))
+   ])
+   pipe_logistic_pca.fit(X_train, y_train)
+   save_model(pipe_logistic_pca, "logistic_pca.joblib")
 
-   # Random forest
-   model_rf = train_random_forest(X_train_scaled, y_train)
-   save_model(model_rf, "random_forest.joblib")
+   # Random Forest
+   pipe_rf = Pipeline([
+      ('scaler', StandardScaler()),
+      ('classifier', RandomForestClassifier(n_estimators=100, random_state=67, n_jobs=-1))
+   ])
+   pipe_rf.fit(X_train, y_train)
+   save_model(pipe_rf, "random_forest.joblib")
 
-   print("Training complete.")
+   print("Training complete with Pipelines.")
